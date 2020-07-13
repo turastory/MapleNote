eb0d0b062231be5dde0fb9988ce1d05a933bc96b
turastory <soldier4443@gmail.com>
2020-07-13 20:30:55 +0900

    :sparkles: Add includeBuild() to make composite build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 rootProject.name = "Maple Note"
 
+includeBuild("../android-toolbox/build-tools")
+includeBuild("../android-toolbox/build-tools-internal")
+includeBuild("../android-toolbox")
+
 include(":app")
