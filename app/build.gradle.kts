e8e6603af9795e695912446aec51896be9d52b64
turastory <soldier4443@gmail.com>
2020-07-23 17:11:52 +0900

    :sparkles: Rewrite build logic using build-tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,19 @@
+import com.turastory.androidtoolbox.build.CommonVersions
+import com.turastory.androidtoolbox.build.android.configureCommons
+import com.turastory.androidtoolbox.build.androidx
+import com.turastory.androidtoolbox.build.junit
+import com.turastory.androidtoolbox.build.kotlinJvm
+
 plugins {
     id("com.android.application")
     kotlin("android")
 }
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion("29.0.3")
+    configureCommons(1, "1.0")
 
     defaultConfig {
         applicationId = "com.turastory.maplenote"
-        minSdkVersion(23)
-        targetSdkVersion(29)
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -25,17 +25,6 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    sourceSets {
-        sourceSets["main"].java.srcDir("src/main/kotlin")
-        sourceSets["test"].java.srcDirs("src/test/kotlin")
-        sourceSets["androidTest"].java.srcDir("src/androidTest/kotlin")
-    }
-
     buildFeatures {
         viewBinding = true
         dataBinding = true
@@ -43,12 +32,11 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
-    implementation("androidx.core:core-ktx:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-
+    implementation(kotlinJvm(CommonVersions.kotlin))
+    implementation(androidx("core", "ktx", version = CommonVersions.core))
+    implementation(androidx("appcompat", version = CommonVersions.appcompat))
+    implementation(androidx("constraintlayout", version = CommonVersions.constraintLayout))
+    testImplementation(junit(CommonVersions.junit))
+    androidTestImplementation("androidx.test:runner:${CommonVersions.test}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${CommonVersions.espresso}")
 }
