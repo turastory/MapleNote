2319c36a9add5ed35caed931e62f0c206a10a543
turastory <soldier4443@gmail.com>
2020-07-23 17:02:56 +0900

    :bug: Fix includedBuild due to external changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "Maple Note"
 
 includeBuild("../android-toolbox/build-tools")
-includeBuild("../android-toolbox/build-tools-internal")
 includeBuild("../android-toolbox")
 
 include(":app")
